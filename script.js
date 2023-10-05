cfefd62c385701ea49512b03e675e556516abaf3
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-05 16:58:09 +0200

    Add scores up during single play function.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const rockPaperScissors = ['rock', 'paper', 'scissors'];
 let computerChoice;
 let playerChoice;
-let computerScore;
-let playerScore;
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     computerChoice = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
@@ -18,7 +18,6 @@ function getPlayerChoice() {
     if (playerChoice.toLowerCase() === 'rock' ||
     playerChoice.toLowerCase() === 'paper' ||
     playerChoice.toLowerCase() === 'scissors') {
-        console.log(playerChoice);
         return playerChoice
     } else {
         alert('Invalid selection, please choose rock, paper, or scissors.');
@@ -35,30 +34,50 @@ function getPlayerChoice() {
 function playOneGame () {
     getPlayerChoice();
     getComputerChoice();
-    console.log('Computer chooses ' + computerChoice);
-    console.log('Player chooses ' + playerChoice);
     if (playerChoice == computerChoice) {
         alert(`It\'s a tie! ${playerChoice} is the same as ${computerChoice}.`)
     } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
         alert(`You win! ${playerChoice} beats ${computerChoice}.`)
+        playerScore = playerScore + 1;
+        return playerScore;
     } else if (playerChoice == 'paper' && computerChoice == 'rock') {
         alert(`You win! ${playerChoice} beats ${computerChoice}.`)
+        playerScore = playerScore + 1;
+        return playerScore;
     } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
         alert(`You win! ${playerChoice} beats ${computerChoice}.`)
+        playerScore = playerScore + 1;
+        return playerScore;
     } else {
         alert(`You lose! ${computerChoice} beats ${playerChoice}.`)
-
+        computerScore = computerScore + 1;
+        return computerScore;
     }
 }
 
 //I'm learning loops in the next lesson so for now will run the game five times and total up scores to determine winner
 
+function fiveGameWinner () {
+    if (computerScore > playerScore) {
+        alert('Sorry pal, you lost the tournament to this computer with a final score of :' + computerScore + ' - ' + playerScore);
+    } else if (playerScore > computerScore) {
+        alert('Humans rule, you won the tournament against this computer with a final score of ' + playerScore + ' - ' + computerScore);
+    } else {
+        alert('What are the odds?? It\'s a tie :D ' + playerScore + ' - ' + computerScore);
+    }
+}
+
 function gameToFive () {
     playOneGame();
+    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
     playOneGame();
+    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
     playOneGame();
+    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
     playOneGame();
+    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
     playOneGame();
+    fiveGameWinner();
 }
 
 gameToFive();
