c70f77a07b98313d2c57ab3be8965bbd1aa7d067
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-10 14:39:15 +0100

    Remove old input function and replac  with event logic.

--- script.js
+++ script.js
@@ -9,19 +9,7 @@ function getComputerChoice() {
     return computerChoice
 }
 
-function getPlayerChoice() {
-    playerChoice = prompt('Choose rock, paper, or scissors.');
-    if (playerChoice.toLowerCase() === 'rock' ||
-    playerChoice.toLowerCase() === 'paper' ||
-    playerChoice.toLowerCase() === 'scissors') {
-        return playerChoice
-    } else {
-        alert('Invalid selection, please choose rock, paper, or scissors.');
-    }
-}
-
 function playOneGame () {
-    getPlayerChoice();
     getComputerChoice();
     if (playerChoice == computerChoice) {
         alert(`It\'s a tie! ${playerChoice} is the same as ${computerChoice}.`)
@@ -47,35 +35,9 @@ function playOneGame () {
 let buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', playOneGame)
+    button.addEventListener('click', () => {
+        playerChoice = button.id;
+        console.log(playerChoice);
+        playOneGame();
+    }) 
 });
-
-//addEventListener('click', playOneGame);
-
-
-/*
-function fiveGameWinner () {
-    if (computerScore > playerScore) {
-        alert('Sorry pal, you lost the tournament to this computer with a final score of :' + computerScore + ' - ' + playerScore);
-    } else if (playerScore > computerScore) {
-        alert('Humans rule, you won the tournament against this computer with a final score of ' + playerScore + ' - ' + computerScore);
-    } else {
-        alert('What are the odds?? It\'s a tie :D ' + playerScore + ' - ' + computerScore);
-    }
-}
-
-function gameToFive () {
-    playOneGame();
-    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
-    playOneGame();
-    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
-    playOneGame();
-    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
-    playOneGame();
-    alert(computerScore + ' point for Computer and ' + playerScore + ' point for Player.');
-    playOneGame();
-    fiveGameWinner();
-}
-
-gameToFive();
-*/
